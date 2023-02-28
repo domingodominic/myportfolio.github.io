089d8a91949af88999151fee56536411f82b7c1d
domingodominic <116958892+domingodominic@users.noreply.github.com>
2023-02-28 21:33:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector(".menu")
+
+
+const navList = document.querySelector(".menus");
+menuBtn.addEventListener("click", function(){
+
+navList.classList.toggle("leftMe");
+    console.log("hello");
+
+
+})
+
